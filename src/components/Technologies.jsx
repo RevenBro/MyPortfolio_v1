c201ca19ffe5cd53fde0c6e15239107464dfa151
RevenBro <saidakbarov077@gmail.com>
2024-11-18 15:35:25 +0500

    Technologies section done

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,4 +1,5 @@
-import {RiReactjsLine, TbbrandNext  } from "react-icons/ri"
+import {RiReactjsLine, RiJavascriptLine, RiGithubLine  } from "react-icons/ri"
+import {TbBrandNextjs, TbBrandTypescript  } from "react-icons/tb"
 
 const Technologies = () => {
   return (
@@ -6,19 +7,19 @@ const Technologies = () => {
         <h2 className='my-20 text-center text-4xl'>Technologies</h2>
         <div className='flex flex-wrap items-center justify-center gap-4'>
             <div className="rounded-2xl border-4 border-neutral-700 p-4">
-                <RiReactjsLine className="text-5xl text-cyan-400 "/>
+                <RiJavascriptLine className="text-5xl text-yellow-300 "/>
+            </div>
+            <div className="rounded-2xl border-4 border-neutral-700 p-4">
+                <TbBrandTypescript className="text-5xl text-blue-700 "/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-700 p-4">
                 <RiReactjsLine className="text-5xl text-cyan-400 "/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-700 p-4">
-                <RiReactjsLine className="text-5xl text-cyan-400 "/>
+                <TbBrandNextjs className="text-5xl "/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-700 p-4">
-                <RiReactjsLine className="text-5xl text-cyan-400 "/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-700 p-4">
-                <RiReactjsLine className="text-5xl text-cyan-400 "/>
+                <RiGithubLine    className="text-5xl  "/>
             </div>
         </div>
     </div>
